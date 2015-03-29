bb1ba49b7d845b34804afd0cb0af76bc6959e9fb
Jesper Haug Karsrud <jesper.karsrud@gmail.com>
2015-03-29 17:49:28 +0200

    Better small device styling

--- app/components/list-item.js
+++ app/components/list-item.js
@@ -3,7 +3,7 @@ import layout from '../templates/components/list-item';
 
 export default Ember.Component.extend({
   layout: layout,
-  classNames: ['list-item', 'col-sm-2', 'col-md-2', 'col-lg-2'],
+  classNames: ['list-item', 'col-xs-4', 'col-sm-3', 'col-md-2', 'col-lg-2'],
   releaseYear: function() {
     return this.get('model.release_date').split('-')[0];
   }.property('model.release_date')
